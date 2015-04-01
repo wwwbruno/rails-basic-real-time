b379ac0df62cc6a1a29a056a184a7f0326d1e2cf
Bruno Almeida <bruno@disvolvi.com>
2015-03-31 23:10:23 -0300

    Angular implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require socket.io
+//= require angular
 //= require_tree .
--- app/assets/javascripts/athletes.js
+++ app/assets/javascripts/athletes.js
@@ -1,9 +1,44 @@
-$(function(){
-  var socket = io.connect('//localhost:3001/');
+(function() {
 
-  socket.on('message', function (data) {
-    data = JSON.parse(data);
-    console.log(data);
-  });
+	var app = angular.module('disvolvi', [])
+	.factory('socket', ['$rootScope', function ($rootScope) {
 
-});
+		var socket = io.connect('//localhost:3001/')
+
+		return {
+			on: function (eventName, callback) {
+				socket.on(eventName, function () {  
+					var args = arguments;
+					$rootScope.$apply(function () {
+						callback.apply(socket, args);
+					});
+				});
+			},
+			emit: function (eventName, data, callback) {
+				socket.emit(eventName, data, function () {
+					var args = arguments;
+					$rootScope.$apply(function () {
+						if (callback) {
+							callback.apply(socket, args);
+						}
+					});
+				})
+			}
+		};
+	}]);
+
+	app.controller('SportsController', ['$scope', '$http', 'socket', function($scope, $http, socket){
+
+		$scope.athletes = []
+		$http.get('/athletes.json').success(function(data){
+	        $scope.athletes = data;
+	    })
+
+	    socket.emit('message')
+
+		socket.on('message', function (data) {
+			data = JSON.parse(data)
+			$scope.athletes.push(data)
+		})
+	}])
+})()
